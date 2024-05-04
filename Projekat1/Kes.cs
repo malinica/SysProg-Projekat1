dfa9199a391d4d524cb39d681d3227443be73862
unknown <malinovicjovana13@gmail.com>
2024-05-05 01:20:17 +0200

    Zapocet je kes

---
+++ Projekat1/Kes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public struct FileAndDate
+{
+    public byte[] bytes;
+    public DateTime created;
+};
+public class Kes
+{
+    private readonly ReaderWriterLockSlim _kesLock; 
+    private readonly Dictionary<string, FileAndDate> _kes;
+    private const int kesKapacitet = 100;
+
+    public Kes()
+    {
+        _kesLock = new ReaderWriterLockSlim();
+        _kes = new Dictionary<string, FileAndDate>(kesKapacitet);
+    }
+}
+
