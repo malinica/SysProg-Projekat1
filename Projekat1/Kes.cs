e053d208934663cb1713c0c383eeffe28be8cd8b
unknown <malinovicjovana13@gmail.com>
2024-05-05 12:33:06 +0200

    Dodate funkcije DodajUKes i StampajSadrzajKesa

--- Projekat1/Kes.cs
+++ Projekat1/Kes.cs
@@ -18,5 +18,40 @@ public class Kes
         _kesLock = new ReaderWriterLockSlim();
         _kes = new Dictionary<string, FileAndDate>(kesKapacitet);
     }
+
+    public void DodajUKes(string key, byte[] value, int timeout)
+    {
+        if (!_kesLock.TryEnterWriteLock(timeout)) return; 
+
+        if (_kes.ContainsKey(key))
+        {
+            throw new Exception("Element je vec u kesu.\n");
+        }
+        FileAndDate fileAndDate = new FileAndDate()
+        {
+            bytes = value,
+            created = DateTime.UtcNow
+        };
+        _kes.Add(key, fileAndDate);
+        Console.WriteLine("UPISANO U KES\n");
+        _kesLock.ExitWriteLock();
+    }
+
+    public void StampajSadrzajKesa()
+    {
+        _kesLock.EnterReadLock();
+        try
+        {
+            Console.WriteLine("Sadrzaj kesa:\n");
+            foreach (var k in _kes)
+            {
+                Console.WriteLine($"Kljuc: {k.Key}, Vrednost: {BitConverter.ToString(k.Value.bytes)}, Kreirano: {k.Value.created}\n");
+            }
+        }
+        finally
+        {
+            _kesLock.ExitReadLock();
+        }
+    }
 }
 
